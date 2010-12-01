1e028536a19dda83a65a6b7eeafb9e1ed8246cab
jebradwell <jebradwell@gmail.com>
2010-11-30 23:26:49 -0800

    Added Page Ranker files

---
+++ huffman/Ranker/CS 453 Project 3/DocData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS_453_Project_3
+{
+    class DocData
+    {
+        public DocData(){
+            linksIn = new List<int>();
+        }
+        private int linksOutCount;
+        private double pageRank;
+        private List<int> linksIn;
+
+
+        public int LinksOutCount
+        {
+            get
+            {
+                return linksOutCount;
+            }
+            set
+            {
+                linksOutCount = value;
+            }
+        }
+
+        public double PageRank
+        {
+            get
+            {
+                return pageRank;
+            }
+            set
+            {
+                pageRank = value;
+            }
+        }
+
+        public List<int> LinksIn
+        {
+            get
+            {
+                return linksIn;
+            }
+        }
+    }
+}
